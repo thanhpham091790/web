1acebc91a2209c228f22159201b34c0a51dd2a9d
Thanh Pham <63334540+thanhpham091790@users.noreply.github.com>
2025-03-04 14:04:12 -0700

    Add burger project.

--- projects/2025009/index.js
+++ projects/2025009/index.js
@@ -1,13 +1,17 @@
 
 
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const nav = document.getElementById('nav');
+const menu = document.querySelector("#menu");
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
 
-open.addEventListener('click', () => {
-    nav.style.transform = 'translateX(0)';
+
+// Open menu when clicked on menu bar
+openMenu.addEventListener("click", () => {
+    menu.style.transform = "translateX(0)";
 });
 
-close.addEventListener('click', () => {
-    nav.style.transform = 'translateX(100%)';
+// Close menu when clicked on X
+closeMenu.addEventListener("click", () => {
+    menu.style.transform = "translateX(100%)";
 });
+
